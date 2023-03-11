649ac1b1a6b1871da3ca0b477e10cc68917a6961
Egor Sazhnev <hard-link@mail.ru>
2023-03-11 19:29:01 +1000

    create task01

---
+++ task01.py
@@ -0,0 +1,14 @@
+# Задача 2: Найдите сумму цифр трехзначного числа.
+# *Пример:*
+# 123 -> 6 (1 + 2 + 3)
+# 100 -> 1 (1 + 0 + 0) 
+
+import os
+os.system('cls')
+
+number = input('Введите трёхзначное число: ')
+a = int(number[0])
+b = int(number[1])
+c = int(number[2])
+result = a+b+c 
+print(result)
